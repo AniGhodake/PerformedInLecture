7110254251ff03fe00ac79be74283b2a53f856fa
Aniket Sunil Ghodake <74956511+AniGhodake@users.noreply.github.com>
2024-01-15 23:12:55 +0530

    Update binarySearchClass2Codes.cpp

--- binarySearchClass2Codes.cpp
+++ binarySearchClass2Codes.cpp
@@ -4,33 +4,33 @@
 #include<vector>
 using namespace std;
 
-int findPivotIndex(vector<int>& arr) {
-        int n = arr.size();
-        int s = 0;
-        int e = n-1;
-        int mid = s + (e-s)/2;
+// int findPivotIndex(vector<int>& arr) {
+//         int n = arr.size();
+//         int s = 0;
+//         int e = n-1;
+//         int mid = s + (e-s)/2;
 
-        while(s <= e) {
-            //corner case
-            if(s == e) {
-                //single element
-                return s;
-            }
+//         while(s <= e) {
+//             //corner case
+//             if(s == e) {
+//                 //single element
+//                 return s;
+//             }
 
-            if(arr[mid] > arr[mid+1])
-                return mid;
-            else if(arr[mid] < arr[mid-1])
-                return mid-1;
-            else if(arr[s] > arr[mid] ) 
-                e = mid - 1;
-            else 
-                s = mid + 1;
+//             if(arr[mid] > arr[mid+1])
+//                 return mid;
+//             else if(arr[mid] < arr[mid-1])
+//                 return mid-1;
+//             else if(arr[s] > arr[mid] ) 
+//                 e = mid - 1;
+//             else 
+//                 s = mid + 1;
             
-            mid = s + (e-s)/2;
-        }
-        return -1;
+//             mid = s + (e-s)/2;
+//         }
+//         return -1;
 
-    }
+//     }
 
 
 int main() {
