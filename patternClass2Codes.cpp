10eaca8d0fa357643ac032b108b806b7a8b94081
Aniket Sunil Ghodake <74956511+AniGhodake@users.noreply.github.com>
2024-01-19 15:03:44 +0530

    Update patternClass2Codes.cpp

--- patternClass2Codes.cpp
+++ patternClass2Codes.cpp
@@ -24,23 +24,23 @@ int main() {
 		cout << endl;
 	}
 
-	// for(int row=0; row<n; row=row+1) {
-	// 	for(int col=0;col<n-row; col=col+1) {
-	// 		if(row ==0 || row == n-1) {
-	// 			cout << "*";	
-	// 		}
-	// 		else {
-	// 			if(col ==0 || col == n-row - 1) {
-	// 				cout <<"*";
-	// 			}
-	// 			else {
-	// 				cout << " ";
-	// 			}
-	// 		}
+	for(int row=0; row<n; row=row+1) {
+		for(int col=0;col<n-row; col=col+1) {
+			if(row ==0 || row == n-1) {
+				cout << "*";	
+			}
+			else {
+				if(col ==0 || col == n-row - 1) {
+					cout <<"*";
+				}
+				else {
+					cout << " ";
+				}
+			}
 				
-	// 	}
-	// 	cout << endl;
-	// }
+		}
+		cout << endl;
+	}
 	
 
 	// for(int row=0; row<n; row=row+1) {
