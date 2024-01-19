cbd1f93328d45fe8a5c748b54250b28aab5ee972
Aniket Sunil Ghodake <74956511+AniGhodake@users.noreply.github.com>
2024-01-19 15:04:49 +0530

    Update patternClass2Codes.cpp

--- patternClass2Codes.cpp
+++ patternClass2Codes.cpp
@@ -76,22 +76,22 @@ int main() {
 
 	// int n = 4;
 
-	// for(int row=0;row<n; row=row+1) {
-	// 	//inverted pyramid 1
-	// 	for(int col=0; col<n-row; col=col+1) {
-	// 		cout << "*";
-	// 	}
-	// 	//full pyramid 1
-	// 	for(int col=0;col<2*row+1; col=col+1) {
-	// 		cout << " ";
-	// 	}
+	for(int row=0;row<n; row=row+1) {
+		//inverted pyramid 1
+		for(int col=0; col<n-row; col=col+1) {
+			cout << "*";
+		}
+		//full pyramid 1
+		for(int col=0;col<2*row+1; col=col+1) {
+			cout << " ";
+		}
 
-	// 	//inverted pyramid 2
-	// 	for(int col=0; col<n-row; col=col+1) {
-	// 		cout << "*";
-	// 	}
-	// 	cout << endl;
-	// }
+		//inverted pyramid 2
+		for(int col=0; col<n-row; col=col+1) {
+			cout << "*";
+		}
+		cout << endl;
+	}
 
 
 	// for(int row=0;row<n; row=row+1) {
