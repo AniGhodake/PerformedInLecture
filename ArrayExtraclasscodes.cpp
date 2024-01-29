e53ff5505c80a840097372cadea770d55bc552e3
Aniket Sunil Ghodake <74956511+AniGhodake@users.noreply.github.com>
2024-01-29 23:50:19 +0530

    Update ArrayExtraclasscodes.cpp

--- ArrayExtraclasscodes.cpp
+++ ArrayExtraclasscodes.cpp
@@ -21,19 +21,19 @@ void shiftNegativeOneSide(int arr[], int n) {
   }
 }
 
-// int main() {
+int main() {
 
-//   int arr[] = {1,7,12,10,11,40,0};
-//   int n = 7;
+  int arr[] = {1,7,12,10,11,40,0};
+  int n = 7;
 
-//   shiftNegativeOneSide(arr, n);
+  shiftNegativeOneSide(arr, n);
 
 
-//   //print
-//   cout << "Printing the array " << endl;
-//   for(int i=0; i<n; i++) {
-//     cout << arr[i] << " ";
-//   }
+  //print
+  cout << "Printing the array " << endl;
+  for(int i=0; i<n; i++) {
+    cout << arr[i] << " ";
+  }
 
 
 //   return 0;
